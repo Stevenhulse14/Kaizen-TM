55ce199e3665042b516882551300453ec9e2da32
steven <shotta3xd@gmail.com>
2026-05-19 12:25:04 -0400

    removed .next/

--- .next/dev/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"kQIJfFingsn6ea5JLI/dUyeNPrBxQnblHeG5RMeFnWY=\"\n}"
--- .next/dev/static/chunks/app_layout_tsx_1cf6b850._.js
+++
@@ -1,7 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/app_index_f1ad76cf.css"
-],
-    source: "dynamic"
-});
